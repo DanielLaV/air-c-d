f3bacddfa89043d6aded8afb16e47125864a5af5
DanielLaV <djjlav@gmail.com>
2022-01-06 22:35:57 -0600

    finished sessionReducer, havent tested yet

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -1,38 +1,71 @@
+/* Actions, Selectors and Reducer for authenticating a session user  */
+import { csrfFetch } from './csrf';
+
+
 export const LOAD_USER = 'LOAD_USER';
 export const REMOVE_USER = 'REMOVE_USER';
 
 
 /* ------- ACTIONS ------- */
 
-export const loadUser = (user) => ({
-    type: LOAD_USER,
-    user: {
-        id,
-        email,
-        username,
-        createdAt,
-        updatedAt
-      }
-})
+export const loadUser = (user) => {
+    const { id, email, username, createdAt, updatedAt } = user;
+    return {
+        type: LOAD_USER,
+        user: {
+            id,
+            email,
+            username,
+            createdAt,
+            updatedAt
+        }
+    }
+};
 
 export const removeUser = (user) => ({
     type: REMOVE_USER,
     user: null
-})
+});
+
+/* ------ SELECTORS ----- */
+
+export const loginUser = (user) => async (dispatch) => {
+    const { id, password } = user;
+    const res = await csrfFetch(`/api/session`, {
+        method: 'POST',
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            credential: id,
+            password
+        })
+    });
+    const data = res.json();
+    console.log('DATA', data)
+    dispatch(loadUser(data));
+}
 
 /* ------ REDUCER ------ */
 
 export default function sessionReducer(state = {}, action) {
+    // const { id, email, username, createdAt, updatedAt } = action.user;
+
     switch (action.type) {
         case LOAD_USER: {
-            return {
-                ...state,
-                user
-            }
+            const newState = Object.assign({}, state);
+            newState.user = {
+                ...action.user,
+                // id,
+                // email,
+                // username,
+                // createdAt,
+                // updatedAt
+            };
+            return newState;
         }
         default: {
-            user: null
+            const newState = Object.assign({}, state);
+            newState.user = null;
+            return newState;
         }
-
     }
 }
