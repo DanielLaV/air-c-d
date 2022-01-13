0884ef8a9a9747ef7c8cb3d1ee17fbe73815e074
DanielLaV <djjlav@gmail.com>
2022-01-13 13:11:08 -0600

    added demo button

--- frontend/src/components/LoginFormModal/index.js
+++ frontend/src/components/LoginFormModal/index.js
@@ -1,12 +1,23 @@
 import React, { useState } from 'react';
 import { Modal } from '../../context/Modal';
 import LoginForm from './LoginForm';
+import { loginUser } from '../../store/session';
+import { useDispatch } from 'react-redux';
 
 function LoginFormModal() {
   const [showModal, setShowModal] = useState(false);
+  const dispatch = useDispatch();
+
+  const demoLogin = () => {
+    const credential = 'demo@user.io';
+    const password = 'password';
+    const user = { credential, password };
+    return dispatch(loginUser(user));
+  }
 
   return (
     <>
+      <button className='loginButton' onClick={demoLogin}>Demo User</button>
       <button className='loginButton' onClick={() => setShowModal(true)}>Log In</button>
       {showModal && (
         <Modal onClose={() => setShowModal(false)}>
