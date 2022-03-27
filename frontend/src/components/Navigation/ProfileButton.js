e9552c9a2b9f2eb503058ea341f3a086e96ef4ae
DanielLaV <djjlav@gmail.com>
2022-03-27 18:30:43 -0500

    profile dropdown menu styled

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -33,7 +33,7 @@ function ProfileButton({ user }) {
 
   return (
     <>
-      <button onClick={openMenu}>
+      <button className="profileButton" onClick={openMenu}>
         <i className="fas fa-user-circle" />
       </button>
       {showMenu && (
